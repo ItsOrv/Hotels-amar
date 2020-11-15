09eec3ea01c29e8d771505454fe8c2a88bc4d6bc
ItsOrv <poriya.saw@gmail.com>
2020-11-15 18:19:08 +0330

    open amar page in a new tab after login

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import config
 import login
 
-CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
-CHROMIUM_PATH = "/usr/bin/google-chrome-stable"
+def main():
+    chrome_options = Options()
+    chrome_options.binary_location = config.CHROMIUM_PATH
+    chrome_options.add_experimental_option("detach", True)
 
-chrome_options = Options()
-chrome_options.binary_location = CHROMIUM_PATH
-chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(executable_path=config.CHROMEDRIVER_PATH, options=chrome_options)
 
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+    driver.get('https://myst.mcth.ir/login.aspx')
 
-driver.get('https://myst.mcth.ir/login.aspx')
+    login.login_to_site(driver)
+    login.open_amar_page(driver)
 
-login.login_to_site(driver)
+if __name__ == "__main__":
+    main()
