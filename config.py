073c3ab1fa8fcfd3cef64fab7755195392a15e02
ItsOrv <poriya.saw@gmail.com>
2020-11-23 22:08:56 +0330

    move paths and wait time into config

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+CHROMIUM_PATH = "/usr/bin/google-chrome-stable"
+
+DATE_FILE = os.path.join(os.getcwd(), "date.txt")
+
+DEFAULT_WAIT_TIME = 10
