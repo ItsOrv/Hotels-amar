b1af7c775a71bb696b9d8da69961de2c874ac39e
ItsOrv <poriya.saw@gmail.com>
2020-12-08 19:27:55 +0330

    write failed rows back to the file so they get retried

--- data_handler.py
+++ data_handler.py
@@ -1,3 +1,8 @@
 def read_date_file(file_path):
     with open(file_path, "r") as file:
         return [line.strip() for line in file.readlines()]
+
+
+def write_date_file(file_path, data):
+    with open(file_path, "a") as file:
+        file.writelines(f"{line}\n" for line in data)
--- registration.py
+++ registration.py
@@ -2,43 +2,56 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
-import algorithms
 from config import DATE_FILE
-
+import algorithms
 
 
 def register_amar(driver):
     count = 0
     while True:
-        wait = WebDriverWait(driver, 10)
-        element = wait.until(EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_lnk_register")))
-        element.click()
+        try:
+            wait = WebDriverWait(driver, 10)
+            element = wait.until(EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_lnk_register")))
+            element.click()
 
-        with open(DATE_FILE) as file:
-            lines = file.readlines()
-            if count < len(lines):
-                line = lines[count].strip()
-            else:
-                break
+            with open(DATE_FILE) as file:
+                lines = file.readlines()
+                if count < len(lines):
+                    line = lines[count].strip()
+                else:
+                    break
 
-        eghamat_random, vorod_random, khoroj_random, otagh_random = algorithms.random_numbers(
-            eghamat_min=1, eghamat_max=10,
-            vorod_min=1, vorod_max=5,
-            khoroj_min=2, khoroj_max=7,
-            otagh_min=3, otagh_max=9,
-        )
+            eghamat_random, vorod_random, khoroj_random, otagh_random = algorithms.random_numbers(
+                eghamat_min=1, eghamat_max=10,
+                vorod_min=1, vorod_max=5,
+                khoroj_min=2, khoroj_max=7,
+                otagh_min=3, otagh_max=9,
+            )
 
-        input_element_1 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[4]/div[1]/input')))
-        input_element_1.clear()
-        input_element_1.send_keys(eghamat_random)
+            input_element_1 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[4]/div[1]/input')))
+            input_element_1.clear()
+            input_element_1.send_keys(eghamat_random)
 
-        input_element = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[3]/div/input')))
-        input_element.clear()
-        input_element.send_keys(line)
-        time.sleep(2)
+            input_element_2 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[7]/div[1]/input')))
+            input_element_2.clear()
+            input_element_2.send_keys(vorod_random)
 
-        submit_element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[3]/input[1]')))
-        submit_element.click()
-        print(line)
-        count += 1
-        time.sleep(2)
+            input_element_3 = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[7]/div[2]/input')))
+            input_element_3.clear()
+            input_element_3.send_keys(khoroj_random)
+
+            input_element = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[1]/div/div[3]/div/input')))
+            input_element.clear()
+            input_element.send_keys(line)
+            time.sleep(2)
+
+            submit_element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[3]/div/div/div[9]/div[2]/div[3]/div/div[1]/div/div[3]/div[3]/input[1]')))
+            submit_element.click()
+            print(line)
+            count += 1
+            time.sleep(2)
+        except Exception as error:
+            print(f"An unexpected error occurred: {error}")
+            with open(DATE_FILE, 'a') as file_to_write:
+                file_to_write.write(line + '\n')
+            continue
